3a0dbb21426a160c75b8d1c4a63cf5d489311bce
hardeepnagi <hardeepnagi@gmail.com>
2017-10-05 10:24:39 +0100

    Just formatting

--- gem/lib/yordex.rb
+++ gem/lib/yordex.rb
@@ -45,12 +45,13 @@ class Yordex
     end
     end
 
-    def master_http(url, method, headers, body=nil) uri = URI.parse(url)
+  def master_http(url, method, headers, body=nil)
+    uri = URI.parse(url)
     printOrLog(url)
     http = Net::HTTP.new(uri.host, uri.port)
     if @@prod==true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl = true
     end
 
     if @@test==true
@@ -81,72 +82,146 @@ class Yordex
     printOrLog(response.to_json)
 
     return response
-    end
+  end
 
-    def get_order(order_id) return master_http(@@api_base+"/orders/#{order_id}", "get", {'Authorization' => @@api_key})
-    end
+  def get_order(order_id)
+    return master_http(@@api_base+"/orders/#{order_id}", "get", {'Authorization'=>@@api_key})
+  end
 
-    def create_trader(partner_id, email, password, company_name, company_address_1, company_city, company_country_code, company_postal_code) _json = if ((company_address_1.nil? || company_address_1.empty?) &&(company_city.nil? || company_city.empty?) &&(company_country_code.nil? || company_country_code.empty?) &&(company_postal_code.nil? || company_postal_code.empty?))
-    then {"partnerId" => partner_id, "user" => {"email" => email, "password" => password
-        }, "companyTradingName" => company_name
-        }
-    else {"partnerId" => partner_id, "user" => {"email" => email, "password" => password
-    }, "companyTradingName" => company_name, "companyTradingAddress" => {"address1" => company_address_1, "city" => company_city, "countryCode" => company_country_code, "postalCode" => company_postal_code
-    }
-    }
-    end
+  def create_trader(partner_id, email, password, company_name, company_address_1, company_city, company_country_code, company_postal_code)
+    _json = if ((company_address_1.nil? || company_address_1.empty?) &&
+                (company_city.nil? || company_city.empty?) &&
+                (company_country_code.nil? || company_country_code.empty?) &&
+                (company_postal_code.nil? || company_postal_code.empty?))
+            then
+              {
+                "partnerId"=>partner_id,
+                "user" =>{
+                  "email"=>email,
+                  "password"=>password
+                },
+                "companyTradingName"=>company_name
+              }
+            else
+              {
+                "partnerId"=>partner_id,
+                "user" =>{
+                  "email"=>email,
+                  "password"=>password
+                },
+                "companyTradingName"=>company_name,
+                "companyTradingAddress"=> {
+                  "address1"=>company_address_1,
+                  "city"=>company_city,
+                  "countryCode"=>company_country_code,
+                  "postalCode"=>company_postal_code
+                }
+              }
+            end
 
-    return master_http(@@api_base+"/traders", "post", {'Authorization' => @@api_key, 'Content-type' => 'application/json', 'Accept' => 'application/json'}, _json)
-    end
+    return master_http(@@api_base+"/traders", "post",
+                      {'Authorization'=>@@api_key, 'Content-type'=>'application/json', 'Accept'=>'application/json'}, _json)
+  end
 
-    def update_trader(trader_id, company_name, address_1, city, country_code, postal_code) _json = if ((address_1.nil? || address_1.empty?) &&(city.nil? || city.empty?) &&(country_code.nil? || country_code.empty?) &&(postal_code.nil? || postal_code.empty?))
-    then {"companyTradingName" => company_name
-        }
-    else {"companyTradingName" => company_name, "companyTradingAddress" => {"address1" => address_1, "city" => city, "countryCode" => country_code, "postalCode" => postal_code
-    }
-    }
-    end
+  def update_trader(trader_id, company_name, address_1, city, country_code, postal_code)
+    _json = if ((address_1.nil? || address_1.empty?) &&
+                (city.nil? || city.empty?) &&
+                (country_code.nil? || country_code.empty?) &&
+                (postal_code.nil? || postal_code.empty?))
+            then
+              {
+                "companyTradingName"=>company_name
+              }
+            else
+              {
+                "companyTradingName"=>company_name,
+                "companyTradingAddress"=> {
+                  "address1"=>address_1,
+                  "city"=>city,
+                  "countryCode"=>country_code,
+                  "postalCode"=>postal_code
+                }
+              }
+            end
 
-    return master_http(@@api_base+"/traders/"+trader_id, "put", {'Authorization' => @@api_key, 'Content-type' => 'application/json', 'Accept' => 'application/json'}, _json)
-    end
+    return master_http(@@api_base+"/traders/"+trader_id, "put",
+                      {'Authorization'=>@@api_key, 'Content-type'=>'application/json', 'Accept'=>'application/json'}, _json)
+  end
 
-    def create_order(buyer_id, seller_id, description, order_amount_in_cents, order_currency, terms) return master_http(@@api_base+"/orders", "post", {'Authorization' => @@api_key, 'Content-type' => 'application/json'}, {"buyerId" => buyer_id, "sellerId" => seller_id, "description" => description, "orderAmountInCents" => order_amount_in_cents, "orderCurrency" => order_currency, "terms" => terms
-    })
-    end
+  def create_order(buyer_id, seller_id, description, order_amount_in_cents, order_currency, terms)
+    return master_http(@@api_base+"/orders", "post",
+                      {'Authorization'=>@@api_key, 'Content-type'=>'application/json'},
+                      {
+                        "buyerId"=>buyer_id,
+                        "sellerId"=>seller_id,
+                        "description"=>description,
+                        "orderAmountInCents"=>order_amount_in_cents,
+                        "orderCurrency"=>order_currency,
+                        "terms"=>terms
+                      })
+  end
 
-    def update_order(order_id, buyer_id, seller_id, description, order_amount_in_cents, order_currency, terms) return master_http(@@api_base+"/orders/"+order_id, "put", {'Authorization' => @@api_key, 'Content-type' => 'application/json'}, {"buyerId" => buyer_id, "sellerId" => seller_id, "description" => description, "orderAmountInCents" => order_amount_in_cents, "orderCurrency" => order_currency, "terms" => terms
-    })
-    end
+  def update_order(order_id, buyer_id, seller_id, description, order_amount_in_cents, order_currency, terms)
+    return master_http(@@api_base+"/orders/"+order_id, "put",
+                      {'Authorization'=>@@api_key, 'Content-type'=>'application/json'},
+                      {
+                        "buyerId"=>buyer_id,
+                        "sellerId"=>seller_id,
+                        "description"=>description,
+                        "orderAmountInCents"=>order_amount_in_cents,
+                        "orderCurrency"=>order_currency,
+                        "terms"=>terms
+                      })
+  end
 
-    def open_order(order_id) return master_http(@@api_base+"/orders/"+order_id+"/open", "post", {'Authorization' => @@api_key})
-    end
+  def open_order(order_id)
+    return master_http(@@api_base+"/orders/"+order_id+"/open", "post", {'Authorization'=>@@api_key})
+  end
 
-    def close_order(order_id) return master_http(@@api_base+"/orders/"+order_id+"/close", "post", {'Authorization' => @@api_key})
-    end
+  def close_order(order_id)
+    return master_http(@@api_base+"/orders/"+order_id+"/close", "post", {'Authorization'=>@@api_key})
+  end
 
-    def approve_order(order_id) return master_http(@@api_base+"/orders/"+order_id+"/approvals", "post", {'Authorization' => @@api_key, 'Content-type' => 'application/json'}, {"approved" => true
-    })
-    end
+  def approve_order(order_id)
+    return master_http(@@api_base+"/orders/"+order_id+"/approvals", "post",
+                      {'Authorization'=>@@api_key, 'Content-type'=>'application/json'},
+                      {
+                          "approved"=>true
+                      })
+  end
 
-    def reject_order(order_id) return master_http(@@api_base+"/orders/"+order_id+"/approvals", "post", {'Authorization' => @@api_key, 'Content-type' => 'application/json'}, {"approved" => false
-    })
-    end
+  def reject_order(order_id)
+    return master_http(@@api_base+"/orders/"+order_id+"/approvals", "post",
+                      {'Authorization'=>@@api_key, 'Content-type'=>'application/json'},
+                      {
+                          "approved"=>false
+                      })
+  end
 
-    def request_advertisements(order_id) return master_http(@@api_base+"/ads?orderId="+order_id, "get", {'Authorization' => @@api_key})
-    end
+  def request_advertisements(order_id)
+    return master_http(@@api_base+"/ads?orderId="+order_id, "get", {'Authorization'=>@@api_key})
+  end
 
-    def enable_advertisement(url) return master_http(url, "post", {'Authorization' => @@api_key})
-    end
+  def enable_advertisement(url)
+    return master_http(url, "post", {'Authorization'=>@@api_key})
+  end
 
-    def disable_advertisement(url) return master_http(url, "delete", {'Authorization' => @@api_key})
-    end
+  def disable_advertisement(url)
+    return master_http(url, "delete", {'Authorization'=>@@api_key})
+  end
 
-    def confirm_event(order_id, event_id) return master_http(@@api_base+"/orders/"+order_id+"/events/"+event_id+"/confirmations", "post", {'Authorization' => @@api_key})
-    end
+  def confirm_event(order_id, event_id)
+    return master_http(@@api_base+"/orders/"+order_id+"/events/"+event_id+"/confirmations", "post", {'Authorization'=>@@api_key})
+  end
 
-    def get_sso_token(uri="/orders/order-id", redirect_url="your-redirect-url") return master_http(@@api_base+"/ssotokens", "post", {'Authorization' => @@api_key, 'Content-type' => 'application/json'}, {"uri" => uri, "redirectUrl" => redirect_url
-    })
-    end
+  def get_sso_token(uri="/orders/order-id", redirect_url="your-redirect-url")
+    return master_http(@@api_base+"/ssotokens", "post",
+                      {'Authorization'=>@@api_key, 'Content-type'=>'application/json'},
+                      {
+                        "uri"=>uri,
+                        "redirectUrl"=>redirect_url
+                      })
+  end
 
 
 end
